4de2292782cf4b3587cdd1595697cacb9e68f792
justmirshod <98507846+justmirshod@users.noreply.github.com>
2022-12-05 23:43:23 +0500

    modified

--- src/Components/StudentList/StudentList.js
+++ src/Components/StudentList/StudentList.js
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import { read_cookie } from "sfcookies";
 
 export default function StudentList() {
-  const { students, activeGroupId, isLoading2 } = useSelector(
+  const { students, activeGroupId, activeDate, isLoading2 } = useSelector(
     (state) => state.groups
   );
   const dispatch = useDispatch();
@@ -15,84 +15,121 @@ export default function StudentList() {
   }, []);
 
   return (
-    <div className="student_list p-5 shadow-2xl border w-4/5 mx-auto">
+    <div className="student_list px-5 py-7 w-[90%] mx-auto rounded-xl mt-10">
       {isLoading2 ? (
         "Loading..."
       ) : isLoading2 === false ? (
         <>
-          <h1 className="text-lg text-home">Students list</h1>
           {students.total > 0 ? (
-            <table className="table-auto w-full ">
-              <thead>
-                <tr className="border-b">
-                  <th className="p-2 text-left">F.I.O</th>
-                  <th className="text-left">Present</th>
-                  <th className="text-left">Abcent</th>
-                  <th className="text-left">Late</th>
-                </tr>
-              </thead>
-              <tbody>
-                {students.results.map((item, index, arr) => (
-                  <tr
-                    key={item.id}
-                    className={index !== arr.length - 1 ? "border-b" : null}
-                  >
-                    <td className="p-2">
-                      {`${item.first_name} ${item.last_name}`} <br />{" "}
-                      {item.email}
-                    </td>
-                    <td>
-                      <div class="flex items-center mb-4">
-                        <input
-                          id={`default-radio-${item.id}`}
-                          type="radio"
-                          value=""
-                          name={`default-radio-${item.id}`}
-                          className=" check w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                          onChange={(e) => {
-                            console.log(e.target.value);
-                          }}
-                        />
-                        <label
-                          htmlFor={`default-radio-${item.id}`}
-                          className="w-6 h-6 border flex items-center justify-center rounded-sm mt-4 focus:bg-buttonMain"
-                        >
-                          <i class=" hidden fa-solid fa-check"></i>{" "}
-                        </label>
-                      </div>
-                    </td>
-                    <td>
-                      <div class="flex items-center mb-4">
-                        <input
-                          id={`default-radio-${item.id}-${item.id + 1}`}
-                          type="radio"
-                          value=""
-                          name={`default-radio-${item.id}`}
-                          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                          onChange={(e) => {
-                            console.log(e.target.value);
-                          }}
-                        />
-                      </div>
-                    </td>
-                    <td>
-                      <div class="flex items-center mb-4">
-                        <input
-                          id={`default-radio-${item.id}-${item.id + 2}`}
-                          type="radio"
-                          value=""
-                          name={`default-radio-${item.id}`}
-                          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                          onChange={(e) => {
-                            console.log(e.target.value);
-                          }}
-                        />
-                      </div>
-                    </td>
+            <>
+              <h1 className="flex items-center mb-2">
+                <div className="flex items-center mr-4">
+                  <span className="mr-1">Group: </span>
+                  <span className="font-semibold text-lg">{`${students.results[0].group.name}`}</span>
+                </div>
+                <div className="flex items-center">
+                  <span className="mr-1">Date: </span>
+                  <span className="font-semibold text-lg">
+                    {activeDate
+                      .split("-")
+                      .map((item) => +item)
+                      .reverse()
+                      .map((item) =>
+                        item.toString().length > 1
+                          ? item
+                          : "0" + item.toString()
+                      )
+                      .join("-")}
+                  </span>
+                </div>
+              </h1>
+
+              <table className="table-auto w-full ">
+                <thead>
+                  <tr className="border-b">
+                    <th className="p-2 text-left">F.I.O</th>
+                    <th className="text-left">Present</th>
+                    <th className="text-left">Abcent</th>
+                    <th className="text-left">Late</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {students.results.map((item, index, arr) => (
+                    <tr
+                      key={item.id}
+                      className={index !== arr.length - 1 ? "border-b" : null}
+                    >
+                      <td className="p-2">
+                        <div className="leading-tight">{`${item.first_name} ${item.last_name}`}</div>
+                        <div className="leading-tight text-sm text-buttonMain">
+                          {item.email}
+                        </div>
+                      </td>
+                      <td>
+                        <div className="flex items-center mb-4">
+                          <input
+                            id={`default-radio-${item.id}`}
+                            type="radio"
+                            value=""
+                            name={`default-radio-${item.id}`}
+                            className="hidden check w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                            onChange={(e) => {
+                              console.log(e.target.value);
+                            }}
+                          />
+                          <label
+                            htmlFor={`default-radio-${item.id}`}
+                            className="w-7 h-7 pres border rounded-full flex items-center justify-center mt-4 focus:bg-buttonMain"
+                          >
+                            <i className=" text-gray-400 hover:text-white fa-solid fa-check"></i>{" "}
+                          </label>
+                        </div>
+                      </td>
+                      <td>
+                        <div className="flex items-center mb-4">
+                          <input
+                            id={`default-radio-${item.id}-${item.id + 1}`}
+                            type="radio"
+                            value=""
+                            name={`default-radio-${item.id}`}
+                            className="hidden check w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                            onChange={(e) => {
+                              console.log(e.target.value);
+                            }}
+                          />
+                          <label
+                            htmlFor={`default-radio-${item.id}-${item.id + 1}`}
+                            className="w-7 h-7 abcent border rounded-full flex items-center justify-center mt-4 focus:bg-buttonMain"
+                          >
+                            <i className="text-gray-400 hover:text-white fa-solid fa-xmark"></i>{" "}
+                          </label>
+                        </div>
+                      </td>
+                      <td>
+                        <div className="flex items-center mb-4">
+                          <input
+                            id={`default-radio-${item.id}-${item.id + 2}`}
+                            type="radio"
+                            value=""
+                            name={`default-radio-${item.id}`}
+                            className="hidden check w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                            onChange={(e) => {
+                              console.log(e.target.value);
+                            }}
+                          />
+                          <label
+                            htmlFor={`default-radio-${item.id}-${item.id + 2}`}
+                            className=" late w-7 h-7 abcent border rounded-full flex items-center justify-center mt-4 focus:bg-buttonMain"
+                          >
+                            <i className="text-sm text-gray-400 hover:text-white fa-solid fa-hourglass-start"></i>{" "}
+                          </label>
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </>
           ) : (
             "Nothing found"
           )}

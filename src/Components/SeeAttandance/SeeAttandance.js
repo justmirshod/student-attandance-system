c48216b350be1c30f1c10d2bfdb6e6c3999be89a
justmirshod <98507846+justmirshod@users.noreply.github.com>
2022-12-12 00:13:40 +0500

    bug found in useefect and style is increased

--- src/Components/SeeAttandance/SeeAttandance.js
+++ src/Components/SeeAttandance/SeeAttandance.js
@@ -16,20 +16,28 @@ export default function SeeAttandance() {
     studentAttandanceLoading,
     seeStudentList,
     activeGroupStudents,
+    allStudentsLoading,
   } = useSelector((state) => state.see);
-  const { activeGroupId, activeSubject } = useSelector((state) => state.groups);
+  const { activeGroupId, activeSubject, activeDate } = useSelector(
+    (state) => state.groups
+  );
   useEffect(() => {
-    dispatch(
-      fecthSeenStudents({
-        token,
-        attandanceId: attandanceId.filter(
-          (item) => item.subject_id == activeSubject
-        )[0].attendance_id,
-        groupId: activeGroupId,
-      })
-    );
     dispatch(fecthActiveGroupStudents({ token, groupId: activeGroupId }));
   }, []);
+
+  useEffect(() => {
+    if (allStudentsLoading === false) {
+      dispatch(
+        fecthSeenStudents({
+          token,
+          attandanceId: attandanceId.filter(
+            (item) => item.subject_id == activeSubject
+          )[0].attendance_id,
+          groupId: activeGroupId,
+        })
+      );
+    }
+  }, [allStudentsLoading]);
 
   useEffect(() => {
     if (
@@ -54,10 +62,93 @@ export default function SeeAttandance() {
 
   return (
     <div className="see_attandance">
-      {studentAttandanceLoading || !seeStudentList ? (
+      {allStudentsLoading || allStudentsLoading === "" ? (
         "Loading..."
       ) : (
-        <>{seeStudentList.total > 0 ? "Hello world" : "Nothing found"}</>
+        <>
+          {studentAttandanceLoading ? (
+            "Loading..."
+          ) : (
+            <>
+              {activeGroupStudents.total === 0 ? (
+                "Nothing found"
+              ) : (
+                <>
+                  {seeStudentList.total === 0 ? (
+                    "Attandance has not been taken from this group yet!"
+                  ) : (
+                    <>
+                      <div className="students_list">
+                        <h1 className="flex items-center mb-2">
+                          Group:{" "}
+                          <span className="ml-1 font-semibold text-lg">
+                            {activeGroupStudents.results[0].group.name}
+                          </span>
+                        </h1>
+                        <table className="table-auto w-full">
+                          <thead>
+                            <tr className="border-b">
+                              <td className="p-2 font-bold">Full Name</td>
+                              <td className="font-bold">Date</td>
+                              <td className="font-bold text-center">Status</td>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {activeGroupStudents.results.map(
+                              (item, index, arr) => (
+                                <tr
+                                  key={item.id}
+                                  className={`${
+                                    index !== arr.length - 1 ? "border-b" : null
+                                  }`}
+                                >
+                                  <td className="p-2">
+                                    {`${item.first_name} ${item.last_name}`}
+                                  </td>
+                                  <td>{activeDate}</td>
+                                  <td>
+                                    {item.extraData ? (
+                                      <div className="w-full flex justify-center">
+                                        <div
+                                          className={`w-6 h-6 rounded-full flex items-center justify-center ${
+                                            item.extraData.status === "present"
+                                              ? "bg-buttonMain"
+                                              : item.extraData.status ===
+                                                "absent"
+                                              ? "bg-[#f03232]"
+                                              : "bg-[#e4990f]"
+                                          }`}
+                                        >
+                                          <i
+                                            className={`text-white fa-solid text-[12px] ${
+                                              item.extraData.status ===
+                                              "present"
+                                                ? "fa-check"
+                                                : item.extraData.status ===
+                                                  "absent"
+                                                ? "fa-xmark"
+                                                : "fa-hourglass-start"
+                                            }`}
+                                          ></i>
+                                        </div>
+                                      </div>
+                                    ) : (
+                                      "Attandance not taken"
+                                    )}
+                                  </td>
+                                </tr>
+                              )
+                            )}
+                          </tbody>
+                        </table>
+                      </div>
+                    </>
+                  )}
+                </>
+              )}
+            </>
+          )}
+        </>
       )}
     </div>
   );

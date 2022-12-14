b190979088d5911596dda08b9d3d5fc7584396b8
justmirshod <98507846+justmirshod@users.noreply.github.com>
2022-12-14 10:54:06 +0500

    added new history slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import list from "../Components/NewslistItem/news_slice";
 import login from "../Components/TeacherLogin/login_slice";
 import sidebar from "../Components/sidebar_slice";
 import groups from "../Components/check_slice";
 import attandance from "../Components/StudentList/student_slice";
 import see from "../Components/see_slice";
 import update from "../Components/edit_slice";
+import history from "../Pages/OneStudentStatus/studentHistory_slice";
 
 export const store = configureStore({
-  reducer: { list, login, sidebar, groups, attandance, see, update },
+  reducer: { login, sidebar, groups, attandance, see, update, history },
   devTools: process.env.NODE_ENV !== "production",
 });
